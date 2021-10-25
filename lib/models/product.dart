5ea054ede243cc668f8da53bcf1462c1b90e63f3
Eduardo Moreira <Du_du.mba@hotmail.com>
2021-10-25 20:00:14 -0300

    first commit

---
+++ lib/models/product.dart
@@ -0,0 +1,21 @@
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final String price;
+  final String imageUrl;
+  bool isFvorite;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFvorite = false,
+  });
+
+  void toggleFavorite(){
+    isFvorite = !isFvorite;
+  }
+}
