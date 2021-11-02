b3669e6dbc792eaead49f27c13c4670fd7fe4c70
Eduardo Moreira <Du_du.mba@hotmail.com>
2021-11-02 09:39:51 -0300

    create img form

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -10,14 +10,28 @@ class ProductFormPage extends StatefulWidget {
 class _ProductFormPageState extends State<ProductFormPage> {
   final _priceFocus = FocusNode();
   final _descriptionFocus = FocusNode();
-
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
 
   @override
-    void dispose() {
-      super.dispose();
-      _priceFocus.dispose();
-      _descriptionFocus.dispose();
-    }
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(updateImgae);
+    
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _priceFocus.dispose();
+    _descriptionFocus.dispose();
+    _imageUrlFocus.removeListener(updateImgae);
+    _imageUrlFocus.dispose();
+  }
+
+  void updateImgae(){
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,23 +47,59 @@ class _ProductFormPageState extends State<ProductFormPage> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Nome'),
                 textInputAction: TextInputAction.next,
-                onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_priceFocus),
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_priceFocus),
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Preço'),
                 textInputAction: TextInputAction.next,
-                focusNode: _priceFocus, 
+                focusNode: _priceFocus,
                 keyboardType: TextInputType.numberWithOptions(
                   decimal: true,
                 ),
-                onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_descriptionFocus),
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_descriptionFocus),
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Descrição'),
-                textInputAction: TextInputAction.next,
                 focusNode: _descriptionFocus,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Url da Imagem'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      focusNode: _imageUrlFocus,
+                      controller: _imageUrlController,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                      top: 10,
+                      left: 10,
+                    ),
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Informe a Url')
+                        : FittedBox(
+                          child: Image.network(_imageUrlController.text),
+                          fit: BoxFit.cover,
+                        ),
+                  ),
+                ],
               ),
             ],
           ),
