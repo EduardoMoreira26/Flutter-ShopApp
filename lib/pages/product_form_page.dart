ebd9259e19117d0e4c9a3c4d485fd52ec8cd565d
Eduardo Moreira <Du_du.mba@hotmail.com>
2021-11-02 08:55:57 -0300

    form

--- lib/pages/product_form_page.dart
+++ lib/pages/product_form_page.dart
@@ -9,6 +9,15 @@ class ProductFormPage extends StatefulWidget {
 
 class _ProductFormPageState extends State<ProductFormPage> {
   final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+
+
+  @override
+    void dispose() {
+      super.dispose();
+      _priceFocus.dispose();
+      _descriptionFocus.dispose();
+    }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +42,15 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 keyboardType: TextInputType.numberWithOptions(
                   decimal: true,
                 ),
-              )
+                onFieldSubmitted: (_) => FocusScope.of(context).requestFocus(_descriptionFocus),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Descrição'),
+                textInputAction: TextInputAction.next,
+                focusNode: _descriptionFocus,
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+              ),
             ],
           ),
         ),
