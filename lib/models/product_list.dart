da1b333a9f2d52deecc7333463aa83acf3f51217
Eduardo Moreira <Du_du.mba@hotmail.com>
2021-11-04 14:43:16 -0300

    commit

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -7,7 +7,7 @@ import 'package:shop/data/dummy_data.dart';
 import 'package:shop/models/product.dart';
 
 class ProductList with ChangeNotifier {
-  final _baseUrl = 'https://shop-cod3r-39781-default-rtdb.firebaseio.com';
+  final _baseUrl = 'https://shop-app-a6bd8-default-rtdb.firebaseio.com/';
   List<Product> _items = dummyProducts;
 
   List<Product> get items => [..._items];
@@ -36,9 +36,9 @@ class ProductList with ChangeNotifier {
     }
   }
 
-  Future<void> addProduct(Product product) {
-    final future = http.post(
-      Uri.parse('$_baseUrl/products'),
+  Future<void> addProduct(Product product) async {
+    final response = await http.post(
+      Uri.parse('$_baseUrl/products.json'),
       body: jsonEncode(
         {
           "name": product.name,
@@ -50,18 +50,16 @@ class ProductList with ChangeNotifier {
       ),
     );
 
-    return future.then<void>((response) {
-      final id = jsonDecode(response.body)['name'];
-      _items.add(Product(
-        id: id,
-        name: product.name,
-        description: product.description,
-        price: product.price,
-        imageUrl: product.imageUrl,
-        isFavorite: product.isFavorite,
-      ));
-      notifyListeners();
-    });
+    final id = jsonDecode(response.body)['name'];
+    _items.add(Product(
+      id: id,
+      name: product.name,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+      isFavorite: product.isFavorite,
+    ));
+    notifyListeners();
   }
 
   Future<void> updateProduct(Product product) {
